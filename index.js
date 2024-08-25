ba844ddc9754c97ec8496589ba2136ad839d331e
muhammadahsanmirza <ahsanmirxa032@gmail.com>
2024-08-25 20:05:27 +0500

    Basic Setup for Clerk Node Js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { verifyToken } = require("@clerk/clerk-sdk-node");
+
+const app = express();
+
+app.post("/protected-route", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const verifiedToken = await verifyToken(token);
+
+    // Now, you can use `verifiedToken` to access user data and roles
+    res.status(200).json({ message: "Access granted", user: verifiedToken });
+  } catch (err) {
+    res.status(401).json({ error: "Unauthorized" });
+  }
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
